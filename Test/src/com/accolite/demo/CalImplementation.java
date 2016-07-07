9a2d8a58f2a44b79a10b7275a61e47cb532240b3
K.Suryateja Rao <surya.teja@accoliteindia.com>
2016-07-07 11:31:06 +0530

    Hi,Welcome to GitHub

---
+++ Test/src/com/accolite/demo/CalImplementation.java
@@ -0,0 +1,76 @@
+package com.accolite.demo;
+
+import java.util.Scanner;
+
+public class CalImplementation implements Calculator {
+
+	@Override
+	public Integer add(Integer a, Integer b) {
+		// TODO Auto-generated method stub
+		return a + b;
+	}
+
+	@Override
+	public Integer sub(Integer a, Integer b) {
+		// TODO Auto-generated method stub
+		return a - b;
+	}
+
+	@Override
+	public Long mul(Integer a, Integer b) {
+		// TODO Auto-generated method stub
+		return (long) a * b;
+	}
+
+	@Override
+	public Integer div(Integer a, Integer b) throws ArithmeticException {
+		// TODO Auto-generated method stub
+		return a/b;
+	}
+	
+	public static void main(String[]args){
+		Calculator calculator = new CalImplementation();
+		Integer a,b,choice;
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Enter a");
+		a = in.nextInt();
+		
+		System.out.println("Enter b");
+		b=in.nextInt();
+		
+		System.out.println("choice:\n1. Add\n2. Sub\n3. Mul\n4. Div\n");
+		choice = in.nextInt();
+		
+		switch (choice) {
+		case 1 :
+			 System.out.println(calculator.add(a, b));
+			 break;
+		case 2: 
+			 System.out.println(calculator.sub(a,b));
+			 break;
+		case 3:
+			System.out.println(calculator.mul(a, b));
+			break;
+		case 4:
+			try{
+				System.out.println(calculator.div(a, b));
+			}
+			catch(ArithmeticException e){
+				e.printStackTrace();
+			}
+			break;
+
+		default:
+			break;
+		}
+		
+	}
+
+
+
+		
+		
+	} 
+	
+
---
+++ Test/src/com/accolite/demo/Calculator.java
@@ -0,0 +1,9 @@
+package com.accolite.demo;
+
+public interface Calculator {
+	public Integer add(Integer a,Integer b);
+	public Integer sub(Integer a,Integer b);
+	public Long    mul(Integer a,Integer b);
+	public Integer div(Integer a,Integer b) throws ArithmeticException;
+	
+}
